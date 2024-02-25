967f4dbe7c7ad8b98cde07bcf7b08d15078f1ef7
Akhilstudent <akhilshan.11@gmail.com>
2024-02-25 19:24:45 +0530

    TestNG CRUD

--- src/test/java/com/TheTestingAcademy/tests/crud/GET/GETRequestNonBDDTestNGbyme.java
+++ src/test/java/com/TheTestingAcademy/tests/crud/GET/GETRequestNonBDDTestNGbyme.java
@@ -1,16 +1,18 @@
 package com.TheTestingAcademy.tests.crud.GET;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.http.Header;
 import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.Test;
 
-public class GETRequestNonBDDTestNG {
+public class GETRequestNonBDDTestNGbyme {
 
     @Test
     public void testGetAllBookingPositive(){
 
         RequestSpecification r = RestAssured.given();
         r.baseUri("https://restful-booker.herokuapp.com");
-        r.basePath("/booking/5566");
+        r.basePath("/booking/5566").log().all();
         r.when().get();
         r.then().log().all().statusCode(200);
     }
@@ -19,7 +21,7 @@ public class GETRequestNonBDDTestNG {
     public void testGetAllBookingNegative(){
         RequestSpecification r = RestAssured.given();
         r.baseUri("https://restful-booker.herokuapp.com");
-        r.basePath("/booking/-1");
+        r.basePath("/booking/-1").log().all();
         r.when().get();
 
         r.then().log().all().statusCode(404);
@@ -29,7 +31,7 @@ public class GETRequestNonBDDTestNG {
     public void testGetAllBookingNegative2(){
         RequestSpecification r = RestAssured.given();
         r.baseUri("https://restful-booker.herokuapp.com");
-        r.basePath("/booking/abc");
+        r.basePath("/booking/abc").log().all();
         r.when().get();
 
         r.then().log().all().statusCode(404);
